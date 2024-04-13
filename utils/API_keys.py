3e75e9a5ba90a8ba31d70fab90d384dd9883132e
edwarddramirez <edwarddramirez@hotmail.com>
2024-04-13 12:47:40 -0400

    API Error Check Added

--- utils/API_keys.py
+++ utils/API_keys.py
@@ -1,2 +1,6 @@
 def get_opensecrets_key():
-    return ""
+    user_key = "[add id here]"
+    if user_key == "[add id here]":
+        raise ValueError("You need to add your OpenSecrets API key in the API_keys.py file")
+    else:
+        return user_key
